a1d3e985cf456c4776d63d3dc5e9214850fb0da6
Shatrox <kevinad.marques@gmail.com>
2026-03-26 19:53:53 +0000

    Front-end implementations

---
+++ Analogix_Backend_App.Infrastructure/Migrations/20260326110556_updateDbHome.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Analogix_Backend_App.Infrastructure.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateDbHome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- Analogix_Backend_App/Program.cs
+++ Analogix_Backend_App/Program.cs
@@ -91,6 +91,17 @@ builder.Services.AddOpenApi(options =>
 
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+
+        policy.WithOrigins("http://localhost:5175")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -99,6 +110,9 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
     app.MapScalarApiReference();
 }
+
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
