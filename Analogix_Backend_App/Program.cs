ee2b4a078e772f16576c0ef06ae9fdab15dfff4e
Shatrox <kevinad.marques@gmail.com>
2026-03-27 16:07:59 +0100

    PlayerProfile update

--- Analogix_Backend_App/Program.cs
+++ Analogix_Backend_App/Program.cs
@@ -96,7 +96,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
 
-        policy.WithOrigins("http://localhost:5175")
+        policy.WithOrigins("http://localhost:5173")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
